68556d741d07509495b4151a4fe4b9f4292672a4
Ben Drucker <bvdrucker@gmail.com>
2015-08-23 17:51:09 -0700

    Tests

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 module.exports = function positionDifference (element1, element2) {
   return {
     x: element2.offsetLeft - element1.offsetLeft,
-    y: element2.offsetTop - element2.offsetTop
+    y: element2.offsetTop - element1.offsetTop
   }
 }
--- test.js
+++ test.js
@@ -4,5 +4,11 @@ var test = require('tape')
 var positionDiff = require('./')
 
 test(function (t) {
-
+  var element1 = {offsetLeft: 1, offsetTop: 2}
+  var element2 = {offsetLeft: 10, offsetTop: 20}
+  t.deepEqual(positionDiff(element1, element2), {
+    x: 9,
+    y: 18
+  })
+  t.end()
 })
